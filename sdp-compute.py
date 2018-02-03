e78550b6c41de26d79cb1c57403e086e02b45d1a
Matthew McCarrison <s1515679@sms.ed.ac.uk>
2018-02-03 20:56:07 +0000

    added post functionality

--- sdp-compute.py
+++ sdp-compute.py
@@ -1,9 +1,35 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
 
-@app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+UPLOAD_FOLDER = 'classify'
+ALLOWED_EXTENSIONS = set(['jpg', 'png', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
+    return """
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    <p>%s</p>
+    """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
